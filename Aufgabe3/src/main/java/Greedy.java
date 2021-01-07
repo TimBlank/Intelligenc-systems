7df9f034ede3f350bfe60fd50e1e0fb0a48befef
TimBlank <tim-behrens@gmx.net>
2021-01-07 14:13:32 +0100

    TODOS eingefügt

--- Aufgabe3/src/main/java/Greedy.java
+++ Aufgabe3/src/main/java/Greedy.java
@@ -11,12 +11,27 @@ public class Greedy implements Algorithm {
 
     @Override
     public void calculate() {
-        // TODO
+        // TODO - liste mit Operationen der Jobs (Name/id;Maschine;länge) "Unsortiert"
+        unsortetjobs = jobs;
+
+        //TODO - Operation in richtige Re
+
+
+        // TODO - längste Zeit in der Liste finden und auswählen
+
+        // TODO - liste aktualisieren (nächste Operation über job hinzufügen)
+        //get next Operation
+
+
+        // TODO - Wiederholen bis "Unsortiert" keinen eintrag mehr besitzt
+        // TODO - Liste Sortiert ausgeben (Nach Position und Maschine Sortiert)
     }
 
     @Override
     public List<Resource> getResources() {
-        // TODO
+        // TODO - alle vorhandenen Maschinen laden
         return null;
     }
+
+
 }
